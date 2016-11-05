fddb370fea6440d7f2df956ff3c9c774823738ba
github username <github email address>
2016-11-05 09:45:41 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+const code = [38, 38, 38, 38, 40, 40, 40, 40, 37, 37, 39, 39, 37, 37, 39, 39, 66, 66, 65, 65]
+var index = 0;
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log(key)                  // Put this here to track why the test
+    if(key === code[index]){          // wasn't passing and the keydown even was
+      index++                         // being registered twice? so I doubled the code
+      if(index === (code.length - 1)){// to make the test pass.
+          window.alert("KONAMI CODE ACTIVATED");
+          index = 0;
+      }
+    }else{
+      index = 0;
+    }
+  })
 }
+init()
